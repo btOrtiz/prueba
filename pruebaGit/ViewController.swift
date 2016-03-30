62586cee24f94787e9a0baae8ee5d46d06a235c1
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2016-03-29 22:37:13 -0600

    Se agrego un nuevo comentario para validar la SYNC con GIT

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-//
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+//    Este comentario es nuevo para probar la SYNc con GIT
 
 }
 
